c8187a92ca755823d54679deb90e21171bec9861
Juan D. Guzmán <juandelgadog98@gmail.com>
2025-03-14 10:09:54 -0500

    ✨Feat(DeepSeek-1.5B): The model has been changed to one with fewer parameters.

--- text-mining/llm/mining.py
+++ text-mining/llm/mining.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
-model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
