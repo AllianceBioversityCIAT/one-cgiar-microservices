888892447af2e968102a3bb99a659a3bee864204
Daniela Gómez <danielagomezayalde@gmail.com>
2025-04-02 09:47:54 -0500

    ✨ feat(mining): refactor start_consumer to simplify document processing and remove wait_for_index

--- mining-service/src/mq/consumer.py
+++ mining-service/src/mq/consumer.py
@@ -1,38 +1,21 @@
 import time
+from src.llm.vectorize import process_file
 from src.llm.mining import generate_response
-from src.llm.vectorize import process_file, table
+#from src.llm.test import generate
 from src.utils.logger.logger_util import get_logger
 from src.utils.s3.s3_util import download_document_s3, delete_local_file
 
 logger = get_logger()
 
-def wait_for_index(document_name, max_wait=10):
-    import time
-    start = time.time()
-    while time.time() - start < max_wait:
-        try:
-            rows = table.to_list()
-            matches = [row for row in rows if row.get("Namedocument") == document_name]
-            if matches:
-                return True
-        except Exception as e:
-            logger.warning(f"Error while checking index: {e}")
-        time.sleep(0.5)
-    return False
-
-
 def start_consumer():
-    key = "ITR D314 Apr 20 2023.pdf"
+    key = "FiBL Tech Report Jan to Jun 2024.pdf"
     logger.debug("Starting the mining service...")
     download_document_s3("microservice-mining", key)
     was_processed = process_file()
     if was_processed:
         logger.info("File processed successfully.")
-        if wait_for_index(key):
-            generate_response(document_name=key)
-        else:
-            logger.warning("Timeout: vectors not ready yet for search.")
-        #generate_response(document_name=key)
+        time.sleep(5)
+        generate_response(document_name=key)
         #generate()
         delete_local_file(key)
         logger.info("Cleanup complete - document removed from database and local storage")
