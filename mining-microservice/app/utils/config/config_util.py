355bcaabf8f7a90551375648127f84431e9c6a1f
Juan D. Guzmán <juandelgadog98@gmail.com>
2025-04-09 11:52:38 -0500

    :sparkles: feat(config): Add MS_NAME environment variable for microservice identification

--- mining-microservice/app/utils/config/config_util.py
+++ mining-microservice/app/utils/config/config_util.py
@@ -22,3 +22,5 @@ RABBITMQ = {
     "protocol": os.getenv("RABBITMQ_PROTOCOL", "amqps"),
     "queue": os.getenv("RABBITMQ_QUEUE", "cola_test_python")
 }
+
+MS_NAME = os.getenv("MS_NAME", "Mining Microservice")
